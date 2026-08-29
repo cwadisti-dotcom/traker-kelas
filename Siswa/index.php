bb3f00549c1d024ab5434e67d8277cc68db1028b
SmufGallagher <akunsosmedsmuf@gmail.com>
2026-08-29 14:07:25 +0700

    feat: Jumlah semua tugas simple dari guru jadi Jumlah semua tugas simple abiz dari guru

--- Siswa/index.php
+++ Siswa/index.php
@@ -162,7 +162,7 @@ $terlambat = mysqli_num_rows(
 
                 <h2><?= $totalTugas; ?></h2>
                 <h3>Total Tugas</h3>
-                <p>Jumlah semua tugas simple dari guru</p>
+                <p>Jumlah semua tugas simple abiz dari guru</p>
 
             </div>
 
